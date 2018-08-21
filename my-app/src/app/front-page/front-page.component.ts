8ee9ade430375b309893bf85a236e90176ab99d1
S0mar <philippe@kuhse.net>
2018-08-21 17:20:54 +0200

    redesigned frontpage

--- my-app/src/app/front-page/front-page.component.ts
+++ my-app/src/app/front-page/front-page.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+export interface Tile {
+
+  /*
+  each tile has text that it will display, a link to the page that the text describes
+  a size (rows, cols) and a color. Maybe there will also be a logo/icon
+  */
+  text: string;
+  routerLink: string;
+  imgSrc: string;
+  cols: number;
+  rows: number;
+  color: string;
+}
+
 
 @Component({
   selector: 'app-front-page',
@@ -6,6 +20,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./front-page.component.css']
 })
 export class FrontPageComponent implements OnInit {
+
+  /*
+  all tiles that will appear on the frontpage
+  */
+  tiles: Tile[] = [
+    {
+      text: 'Aktuelle Auslastung',
+      routerLink: 'current-utilization',
+      imgSrc: 'desktop_windows',
+      cols: 2,
+      rows: 2,
+      color: 'primary'
+    },
+
+    {
+      text: 'Öffnungszeiten',
+      routerLink: 'opening-hours',
+      imgSrc: 'access_time',
+      cols: 2,
+      rows: 2,
+      color: 'lightblue'
+    },
+
+    {
+      text: 'Reservierungen',
+      routerLink: 'NONE',
+      imgSrc: 'alarm',
+      cols: 2,
+      rows: 2,
+      color: 'lightblue'
+    },
+
+    {
+      text: 'Vorhersage',
+      routerLink: 'prediction',
+      imgSrc: 'bar_chart',
+      cols: 2,
+      rows: 2,
+      color: 'lightblue'
+    },
+/*
+    {
+      text: 'Layout Editor',
+      routerLink: 'NONE',
+      imgSrc: 'NONE',
+      cols: 2,
+      rows: 2,
+      color: 'lightblue'
+    }
+*/
+  ];
 
   constructor() { }
 
--- my-app/src/app/main-nav/main-nav.component.spec.ts
+++ my-app/src/app/main-nav/main-nav.component.spec.ts
@@ -1,5 +1,5 @@
 
-import { fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MainNavComponent } from './main-nav.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
@@ -8,20 +8,21 @@ describe('MainNavComponent', () => {
   let component: MainNavComponent;
   let fixture: ComponentFixture<MainNavComponent>;
 
-  beforeEach(fakeAsync(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatSidenavModule],
-      declarations: [MainNavComponent],
+      declarations: [ MainNavComponent ],
       schemas: [
         NO_ERRORS_SCHEMA //muss drin sein weil sonst nur errors kommen
       ]
     })
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MainNavComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should compile', () => {
     expect(component).toBeTruthy();
