db0c470e51acad563e1a616c0bee112af3db739e
Johannes Ratz <ratz.hannes@googlemail.com>
2018-08-24 18:16:44 +0200

    changed default route to front-page, added 'edit dialog' for opening hours

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { PredictionComponent } from './prediction/prediction.component';
 import { OpeningHoursComponent } from './opening-hours/opening-hours.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { CurrentUtilizationComponent } from './current-utilization/current-utilization.component';
-import { AppComponent } from './app.component';
 import { LayoutEditorComponent } from './layout-editor/layout-editor.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/front-page',
+    pathMatch: 'full'
+  },
   {
     path: 'front-page',
     component: FrontPageComponent
---
+++ my-app/src/app/opening-hours-dialog/opening-hours-dialog.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
+@Component({
+  selector: 'app-opening-hours-dialog',
+  templateUrl: './opening-hours-dialog.component.html',
+  styleUrls: ['./opening-hours-dialog.component.css']
+})
+export class OpeningHoursDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
